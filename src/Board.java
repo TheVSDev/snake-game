64a1bc7d3dec607502cd0a15924959d6cab7c31e
TheVSDev <vspasic03@gmail.com>
2023-09-08 09:33:42 +0200

    done: new Board class, background color

---
+++ src/Board.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Board extends JPanel {
+    Board() {
+        setBackground(Color.BLACK);
+        setFocusable(true);
+    }
+}
--- src/SnakeGame.java
+++ src/SnakeGame.java
@@ -4,6 +4,9 @@ public class SnakeGame extends JFrame {
 
     SnakeGame() {
         super("Snake Game"); // Naming the window
+        add(new Board());
+        pack();
+
         setSize(300, 300); // Setting the size of the window
         setLocationRelativeTo(null); // Centering the window on the screen
         setVisible(true); // Making the window visible and displaying it on the screen
