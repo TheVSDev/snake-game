9689cb5f8e9de97b43cfe68582bf4af15c31ef37
TheVSDev <vspasic03@gmail.com>
2023-09-08 02:21:24 +0200

    update: comment to explain the code

--- src/SnakeGame.java
+++ src/SnakeGame.java
@@ -3,10 +3,10 @@ import javax.swing.*;
 public class SnakeGame extends JFrame {
 
     SnakeGame() {
-        super("Snake Game");
-        setSize(300, 300);
-        setLocationRelativeTo(null);
-        setVisible(true);
+        super("Snake Game"); // Naming the window
+        setSize(300, 300); // Setting the size of the window
+        setLocationRelativeTo(null); // Centering the window on the screen
+        setVisible(true); // Making the window visible and displaying it on the screen
     }
 
     public static void main(String[] args) {
